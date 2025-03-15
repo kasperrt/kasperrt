7f6094ed766db678b7673b04f8819f6175d488fc
Kasper Rynning-Tønnesen <github@bundle.email>
2025-03-15 22:03:42 +0100

    new files

---
+++ app/hooks/useRotate.ts
@@ -0,0 +1,50 @@
+import { type RefObject, useCallback, useEffect } from 'react';
+
+interface Props {
+  elements: {
+    multiplier?: number;
+    shadow?: {
+      multiplier: number;
+    };
+    ref: RefObject<HTMLElement | null>;
+  }[];
+}
+
+export function useRotate({ elements }: Props) {
+  const onMouseMove = useCallback(
+    (e: MouseEvent) => {
+      if (!document) {
+        return;
+      }
+
+      for (const element of elements) {
+        if (!element.ref.current) {
+          continue;
+        }
+        const curr = element.ref.current;
+        const multiplier = element.multiplier ?? 1;
+        const left = (e.clientX / document.documentElement.clientWidth - 0.5) * multiplier;
+        const top = (e.clientY / document.documentElement.clientHeight - 0.5) * multiplier;
+        curr.style.transform = `translate(${left}px, ${top}px)`;
+
+        if (!element.shadow) {
+          continue;
+        }
+        curr.style.boxShadow = `${left * element.shadow.multiplier}px ${top * element.shadow.multiplier}px #8e8e8e`;
+      }
+    },
+    [elements],
+  );
+
+  useEffect(() => {
+    if (!elements?.length) {
+      return;
+    }
+
+    window.addEventListener('mousemove', onMouseMove, { passive: true });
+
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove);
+    };
+  }, [elements.length, onMouseMove]);
+}
