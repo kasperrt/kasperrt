956ebf1885ead14de6a9de3ad223a79430b11f65
Kasper Rynning-Tønnesen <github@bundle.email>
2025-12-17 09:02:51 +0100

    test: Analytics

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script defer data-domain="kasperrt.me" src="https://analytics.kasperrt.me/js/script.js" />
         <Meta />
         <Links />
       </head>
