5fafa123e0d9872ae6ea5d65abc6c05afea5c048
Kasper Rynning-Tønnesen <github@bundle.email>
2025-12-31 01:21:58 +0100

    cleanup

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="astro/client" />
 
-interface ImportMetaEnv {
-  readonly PUBLIC_SENTRY_DSN?: string;
-  readonly PUBLIC_SENTRY_ENV?: string;
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
