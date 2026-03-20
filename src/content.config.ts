ca2d2208b2fa7661c82727a355c41bff26cecde9
Kasper Rynning-Tønnesen <kasper@rynning-toennesen.no>
2026-03-20 10:41:53 +0100

    chore: Package updates

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,16 @@
-import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
 import { blogSchema } from "~/schemas/blog";
 import { educationSchema, experienceSchema, skillsSchema } from "~/schemas/more";
 
 const more = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/more" }),
   schema: z.discriminatedUnion("type", [experienceSchema, skillsSchema, educationSchema]),
 });
 
 const blog = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
   schema: blogSchema,
 });
 
